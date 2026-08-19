56381c0846a5b8af8aed1ab7cfa1eebd97f07663
Md Abrar Faiyaz <163644568+md-abrar-faiyaz@users.noreply.github.com>
2026-08-20 05:54:28 +0600

    Update database.py

--- database.py
+++ database.py
@@ -10,8 +10,6 @@ def get_db_connection():
     Establishes a connection to the MySQL database using environment variables.
     Returns a MySQL connection object.
     
-    Make sure to provide DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, and DB_PORT 
-    in your environment variables or .env file.
     """
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
