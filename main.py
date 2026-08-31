2cc6ae487b9b7829f127f3988b73f7d78878e79c
Orin <orinkhan78@google.com>
2026-08-31 22:18:54 +0600

    tehsis done up3

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
         "https://thesis-hunt.web.app",
         "https://thesis-hunt.firebaseapp.com"
     ],
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:[0-9]+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
